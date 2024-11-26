d7fa1331bad595f98d5de3ba605d1deeb74e39e8
Wanda Valadares WandaWanda1@users.noreply.github.com <wandawanda@gmail.com>
2024-11-26 16:34:42 +0100

    created the patients controller, index, new, edit

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_105954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +33,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_105954) do
     t.boolean "online_patient", default: true
     t.index ["email"], name: "index_patients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
