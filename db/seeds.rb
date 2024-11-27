94e8f5e394c00ef5125b835650a2f6fc5058ed4b
Wanda Valadares WandaWanda1@users.noreply.github.com <wandawanda@gmail.com>
2024-11-27 12:29:31 +0100

    created seeds for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,39 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Categories.destroy_all
+
+Categories.create!([
+  {name: "Urgences cardio-vasculaires"},
+  {name: "Urgences respiratoires"},
+  {name: "Urgences traumatiques"},
+  {name: "Urgences neurologiques"},
+  {name: "Urgences digestives"},
+  {name: "Urgences pediatriques"},
+  {name: "Urgences gynécologiques et obstétricales"},
+  {name: "Urgences psychiatriques"},
+  {name: },
+
+])
+p "Created #{Categories.count} categories"
+
+# Hospitals.destroy_all
+
+# Hospitals.create!([
+#   {name: "Urgences cardio-vasculaires"},
+#   {name: "Urgences respiratoires"},
+#   {name: "Urgences traumatiques"},
+#   {name: "Urgences neurologiques"},
+#   {name: "Urgences digestives"},
+#   {name: "Urgences pediatriques"},
+#   {name: "Urgences gynécologiques et obstétricales"},
+#   {name: "Urgences psychiatriques"},
+#   {name: '', description: ''},
+
+# ])
+
+# Urgences cardio-vasculaires : infarctus, AVC, embolie pulmonaire
+# Urgences respiratoires : asthme sévère, OBSTRUCTION des voies aériennes
+# Urgences traumatiques : fractures, brûlures étendues, plaies profondes
+# Urgences neurologiques : convulsions, méningite, coma
+# Urgences digestives : appendicite, occlusion intestinale, hémorragie digestive
+# Urgences pediatriques
+# Urgences gynécologiques et obstétricales
+# Urgences psychiatriques
