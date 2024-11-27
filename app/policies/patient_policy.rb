7270b265f5faeef659f5dec6891325ef47fa6eda
Erika Belicova <erika.belicov@gmail.com>
2024-11-27 13:00:11 +0100

    Patients pundit controller policy

--- app/policies/patient_policy.rb
+++ app/policies/patient_policy.rb
@@ -11,4 +11,32 @@ class PatientPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end
