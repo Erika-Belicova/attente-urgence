8887a8371a990b1fc6d9025b69b33b6a2a959c1c
Erika Belicova <erika.belicov@gmail.com>
2024-11-27 14:13:31 +0100

    Policy and controller pundit for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
   def index
+    authorize @categories
     @categories = Category.all
   end
 
   def new
+    authorize @category
     @category = Category.new
   end
 
   def create
+    authorize @category
     @category = Category.new(category_params)
     @category.save
   end
