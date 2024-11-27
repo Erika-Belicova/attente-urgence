4797355e4e721bff860f1388eeaaad760e565475
Benoit Lamy <lamybenoit@gmail.com>
2024-11-27 12:07:58 +0100

    adding 's to categories

--- app/models/hospitals_categories.rb
+++ app/models/hospitals_categories.rb
@@ -1,4 +1,4 @@
-class HospitalsCategory < ApplicationRecord
+class HospitalsCategories < ApplicationRecord
   belongs_to :hospital
   belongs_to :category
 end
--- test/models/hospitals_categories_test.rb
+++ test/models/hospitals_categories_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class HospitalsCategoryTest < ActiveSupport::TestCase
+class HospitalsCategoriesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
