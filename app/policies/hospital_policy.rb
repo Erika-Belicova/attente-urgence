bc60ea15291d8c42e388351b919aab25fe710f3b
Erika Belicova <erika.belicov@gmail.com>
2024-11-27 14:28:10 +0100

    policy and controller pundit for hospital

--- app/policies/hospital_policy.rb
+++ app/policies/hospital_policy.rb
@@ -11,4 +11,20 @@ class HospitalPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
 end
